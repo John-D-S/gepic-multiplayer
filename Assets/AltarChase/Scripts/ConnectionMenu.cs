6e87099d22e60d51eeae112f145a949d5e2567e4
John <68535837+John-D-S@users.noreply.github.com>
2021-10-12 12:39:05 +1100

    added connection menu as per lesson

---
+++ Assets/AltarChase/Scripts/ConnectionMenu.cs
@@ -0,0 +1,46 @@
+using Mirror;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using TMPro;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AltarChase
+{
+	public class ConnectionMenu : MonoBehaviour
+	{
+		private NetworkManager networkManager;
+
+		[SerializeField] private Button hostButton;
+		[SerializeField] private TMP_InputField inputField;
+		[SerializeField] private Button connectButton;
+
+		private void Start()
+		{
+			networkManager = NetworkManager.singleton;
+			
+			hostButton.onClick.AddListener(OnClickHost);
+			inputField.onEndEdit.AddListener(OnEndEditAddress);
+			connectButton.onClick.AddListener(OnClickConnect);
+		}
+
+		private void OnClickHost() => networkManager.StartHost();
+		private void OnEndEditAddress(string _value) => networkManager.networkAddress = _value;
+
+		private void OnClickConnect()
+		{
+			string address = inputField.text;
+			if(string.IsNullOrEmpty(address))
+			{
+				address = "localhost";
+			}
+
+			networkManager.networkAddress = address;
+			networkManager.StartClient();
+		}
+	}
+}
