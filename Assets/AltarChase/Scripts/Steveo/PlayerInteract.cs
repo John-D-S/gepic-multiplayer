92fbbed1cdc82fa73af54bfb404aceec8febb0af
John <68535837+John-D-S@users.noreply.github.com>
2021-11-16 22:52:09 +1100

    edited camera move amount and fixed options menu errors

--- Assets/AltarChase/Scripts/Steveo/PlayerInteract.cs
+++ Assets/AltarChase/Scripts/Steveo/PlayerInteract.cs
@@ -300,7 +300,7 @@ namespace AltarChase.Player
         {
 	        if(isLocalPlayer)
 	        {
-		        playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, transform.position + camOffset, 0.025f);
+		        playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, transform.position + camOffset, 0.035f);
 		        playerCamera.transform.LookAt(transform.position, Vector3.forward);
 		        
 		        if(Input.GetKeyDown(KeyCode.Space) || (playerInput.actions["Drop Trap"].triggered))
